6fd1eb9e98c2f67fcafa7c244dc1fcb2893f4839
Meo Leung <misyleung@gmail.com>
2020-04-19 16:25:34 -0500

    1.4: course information step4 Place the objects into an array & lots of refactoring

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -18,46 +18,41 @@ const Part = (props) => {
 
 // Content renders the parts and their number of exercises
 const Content = (props) => {
-  const { part1, part2, part3 } = props;
-  return (
-    <div>
-      <Part part={part1}></Part>
-      <Part part={part2}></Part>
-      <Part part={part3}></Part>
-    </div>
-  );
+  const { parts } = props;
+  return parts.map((part, index) => <Part part={part} key={index}></Part>);
 };
 
 // Total renders the total number of exercises.
 const Total = (props) => {
-  const { exercises1, exercises2, exercises3 } = props;
-  return <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>;
+  const { parts } = props;
+  const totalExercises = parts
+    .map((part) => part.exercises)
+    .reduce((accumulator, currentValue) => accumulator + currentValue);
+  return <p>Number of exercises {totalExercises}</p>;
 };
 
 const App = () => {
   const course = "Half Stack application development";
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10,
-  };
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7,
-  };
-  const part3 = {
-    name: "State of a component",
-    exercises: 14,
-  };
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10,
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7,
+    },
+    {
+      name: "State of a component",
+      exercises: 14,
+    },
+  ];
 
   return (
     <div>
       <Header course={course}></Header>
-      <Content part1={part1} part2={part2} part3={part3}></Content>
-      <Total
-        exercises1={part1.exercises}
-        exercises2={part2.exercises}
-        exercises3={part3.exercises}
-      ></Total>
+      <Content parts={parts}></Content>
+      <Total parts={parts}></Total>
     </div>
   );
 };
